c117dbf8560413217a68c9ab3fd09e19c9766e7c
Spandan Barve <spandan567@gmail.com>
2024-06-06 00:04:41 +0530

    Add live checked in counter to landing page

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -3,8 +3,14 @@
 import Link from "next/link";
 import type { NextPage } from "next";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
 
 const Home: NextPage = () => {
+  const checkedInCounter = useScaffoldReadContract({
+    contractName: "BatchRegistry",
+    functionName: "checkedInCounter",
+  });
+
   return (
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
@@ -16,7 +22,8 @@ const Home: NextPage = () => {
           <p className="text-center text-lg">Get started by taking a look at your batch GitHub repository.</p>
           <p className="text-lg flex gap-2 justify-center">
             <span className="font-bold">Checked in builders count:</span>
-            <span>To Be Implemented</span>
+            <span className={`${!checkedInCounter.isFetched && "hidden"}`}>{Number(checkedInCounter.data)}</span>
+            <span className={`loading ${checkedInCounter.isFetched && "hidden"}`} />
           </p>
         </div>
 
