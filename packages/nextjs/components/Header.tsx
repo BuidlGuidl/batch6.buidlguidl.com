6cb73ad0ad9520ff0de0abd9de999ef500a0d8ce
Osueni Elizabeth <107366414+Okhayeeli@users.noreply.github.com>
2024-06-14 10:14:53 +0100

    Change to Logo / title / subtitle #7 (#23)

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -116,8 +116,8 @@ export const Header = () => {
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">Batch #6</span>
+            <span className="text-xs">bUiDl GuIdL</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
