31f45007fb3e01072ec4088cfb02ea9983ea799a
MayowaObisesan <mayowaobi74@gmail.com>
2024-06-19 22:02:58 +0100

    Clean up page

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -13,11 +13,6 @@ const Home: NextPage = () => {
   const [newGreeting, setNewGreeting] = useState("");
 
   const { writeContractAsync: writeGreetingGreetingAsync, isPending } = useScaffoldWriteContract("Greetings");
-  // , {
-  //   functionName: "setGreeting",
-  //   args: [newGreeting],
-  //   value: "0.01",
-  // });
 
   const handleSendGreeting = async () => {
     try {
@@ -58,7 +53,6 @@ const Home: NextPage = () => {
                 </span>
               </div>
               <div className="stat-actions">
-                {/* <button className="btn btn-sm">See Builders</button> */}
                 <div className="flex justify-start">
                   <Link
                     className="inline-flex items-center gap-x-2 bg-white border border-gray-200 font-medium text-md text-gray-600 p-3 px-3 rounded-full transition hover:border-gray-300 dark:bg-base-300 dark:border-0 dark:hover:border-neutral-600 dark:text-neutral-400"
@@ -92,15 +86,6 @@ const Home: NextPage = () => {
                 </div>
               </div>
             </div>
-
-            {/* <div className="stat">
-            <div className="stat-title">Current balance</div>
-            <div className="stat-value">$89,400</div>
-            <div className="stat-actions">
-              <button className="btn btn-sm">Withdrawal</button>
-              <button className="btn btn-sm">Deposit</button>
-            </div>
-          </div> */}
           </div>
 
           <div className="relative w-full max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10 sm:py-16 mt-8 rounded-3xl bg-base-100">
@@ -110,7 +95,6 @@ const Home: NextPage = () => {
             </div>
 
             <div className="mt-2 sm:mt-6 mx-auto relative">
-              {/* <!-- Form --> */}
               <div>
                 <div className="relative z-10 flex items-center space-x-3 p-3 border rounded-box shadow-lg bg-base-300 dark:border-0">
                   <div className="flex-[1_0_0%] ">
@@ -138,68 +122,10 @@ const Home: NextPage = () => {
                       {isPending ? <span className="loading loading-spinner"></span> : <LucideArrowRight />}
                     </button>
                   </div>
-
-                  {/* <div className="flex-[0_0_auto] ">
-                    <button
-                      className={`size-[46px] inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none ${
-                        isPending ? "loading disabled" : ""
-                      }`}
-                    >
-                      <svg
-                        className="flex-shrink-0 size-5"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      >
-                        <circle cx="11" cy="11" r="8" />
-                        <path d="m21 21-4.3-4.3" />
-                      </svg>
-                    </button>
-                  </div> */}
                 </div>
               </div>
-              {/* <!-- End Form --> */}
               <div className="font-medium text-md my-4">Fee: 0.01 ETH + Gas</div>
 
-              {/* <!-- SVG Element --> */}
-              <div className="md:hidden absolute top-0 end-0 -translate-y-12 translate-x-20">
-                <svg
-                  className="w-16 h-auto text-orange-500"
-                  width="121"
-                  height="135"
-                  viewBox="0 0 121 135"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M5 16.4754C11.7688 27.4499 21.2452 57.3224 5 89.0164"
-                    stroke="currentColor"
-                    strokeWidth="10"
-                    strokeLinecap="round"
-                  />
-                  <path
-                    d="M33.6761 112.104C44.6984 98.1239 74.2618 57.6776 83.4821 5"
-                    stroke="currentColor"
-                    strokeWidth="10"
-                    strokeLinecap="round"
-                  />
-                  <path
-                    d="M50.5525 130C68.2064 127.495 110.731 117.541 116 78.0874"
-                    stroke="currentColor"
-                    strokeWidth="10"
-                    strokeLinecap="round"
-                  />
-                </svg>
-              </div>
-              {/* <!-- End SVG Element --> */}
-
-              {/* <!-- SVG Element --> */}
               <div className="hidden md:block absolute bottom-0 start-0 translate-y-10 -translate-x-32">
                 <svg
                   className="w-40 h-auto text-cyan-500"
@@ -217,25 +143,11 @@ const Home: NextPage = () => {
                   />
                 </svg>
               </div>
-              {/* <!-- End SVG Element --> */}
             </div>
           </div>
         </div>
 
         <ContractData />
-        {/* <div className="flex-1 flex flex-row justify-center items-center bg-base-100 bg-gradient-to-br from-blue-600  to-purple-600">
-          <div>
-            <form className="inline-block bg-gradient-to-tl from-blue-600 via-transparent to-purple-600 p-1 rounded-xl mb-3">
-              <div className="lg:max-w-lg lg:mx-auto lg:me-0 ms-auto">
-                <div className="p-4 sm:p-7 flex flex-col bg-white rounded-2xl shadow-lg dark:bg-neutral-900 overflow-auto">
-                  <div className="mt-3 border border-primary bg-neutral rounded-3xl text-secondary  overflow-hidden text-[116px] whitespace-nowrap w-full uppercase tracking-tighter font-bai-jamjuree leading-tight">
-                    <div className="p-6 text-center text-5xl">{"Shoutout to Batch 6"}</div>
-                  </div>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div> */}
 
         <div className="hidden w-full p-6">
           <h1 className="text-left">
