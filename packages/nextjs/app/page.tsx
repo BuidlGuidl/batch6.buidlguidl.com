bcdb48292e940515bea9cbe20fe0dc916ce2eea2
Mayowa Obisesan <mayowaobi74@gmail.com>
2024-06-25 17:23:28 +0000

    Update main page (#24)
    * Update main page
    
    * Clean up page
    
    * Clean up main page
    
    * Update target networks
    
    * Update deploy contract
    
    * update build file
    
    * add lucide-react
    
    ---------
    
    Co-authored-by: hdevelopergit <hugo.anga19@gmail.com>

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,11 +1,31 @@
 "use client";
 
+import { useState } from "react";
 import Link from "next/link";
+import { LucideArrowRight } from "lucide-react";
 import type { NextPage } from "next";
+import { parseEther } from "viem";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
+import { ContractData } from "~~/components/assets/ContractData";
+import { useScaffoldReadContract, useScaffoldWriteContract } from "~~/hooks/scaffold-eth";
 
 const Home: NextPage = () => {
+  const [newGreeting, setNewGreeting] = useState("");
+
+  const { writeContractAsync: writeGreetingGreetingAsync, isPending } = useScaffoldWriteContract("Greetings");
+
+  const handleSendGreeting = async () => {
+    try {
+      await writeGreetingGreetingAsync({
+        functionName: "setGreeting",
+        args: [newGreeting],
+        value: parseEther("0.1"),
+      });
+    } catch (e) {
+      console.error("Error setting greeting:", e);
+    }
+  };
+
   const checkedInCounter = useScaffoldReadContract({
     contractName: "BatchRegistry",
     functionName: "checkedInCounter",
@@ -13,8 +33,142 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
+      <div className="relative bg-gradient-to-bl from-blue-100 via-transparent dark:from-blue-950 dark:via-transparent flex flex-row flex-grow lg:grid lg:grid-cols-2">
+        <div className="bg-orange-10 flex-1 flex flex-col p-8">
+          <Link
+            href={"/blockexplorer"}
+            title="transaction explorer"
+            className="btn absolute right-8 flex flex-row items-center gap-x-4 w-full max-w-xs bg-base-100 border-base-300 ring-1 rounded-full p-0 z-30"
+          >
+            <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
+            <span>Click here to see your transactions</span>
+          </Link>
+
+          <div className="stats px-4 py-4 dark:bg-transparent">
+            <div className="stat">
+              <div className="stat-title text-lg">Welcome to</div>
+              <div className="stat-value">
+                <span className="inline-block text-5xl font-bold bg-clip-text bg-gradient-to-l from-blue-600 to-violet-500 text-transparent dark:from-blue-400 dark:to-violet-400">
+                  Batch 6
+                </span>
+              </div>
+              <div className="stat-actions">
+                <div className="flex justify-start">
+                  <Link
+                    className="inline-flex items-center gap-x-2 bg-white border border-gray-200 font-medium text-md text-gray-600 p-3 px-3 rounded-full transition hover:border-gray-300 dark:bg-base-300 dark:border-0 dark:hover:border-neutral-600 dark:text-neutral-400"
+                    href="/builders"
+                  >
+                    See our batch builders 😎
+                    <span className="flex items-center gap-x-1">
+                      <span className="flex flex-row items-center border-s border-gray-200 font-bold text-md text-blue-600 ps-3 px-3 dark:text-blue-500 dark:border-neutral-700">
+                        {!checkedInCounter.isFetched ? (
+                          <span className="loading loading-lg loading-ring" />
+                        ) : (
+                          <span>{checkedInCounter?.data?.toString() || 0} builders</span>
+                        )}
+                      </span>
+                      <svg
+                        className="hidden border-s border-gray-200 flex-shrink-0 size-4 text-blue-600 dark:text-blue-500 dark:border-neutral-700"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path d="m9 18 6-6-6-6" />
+                      </svg>
+                    </span>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="relative w-full max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10 sm:py-16 mt-8 rounded-3xl bg-base-100">
+            <div className="relative flex flex-row items-center font-bold text-7xl">Write a Shoutout 📣</div>
+            <div className="relative flex flex-row items-center font-bold text-md p-3 bg-clip-text bg-gradient-to-l from-blue-600 to-violet-500 text-transparent dark:from-blue-400 dark:to-violet-400">
+              and we will display it for you
+            </div>
+
+            <div className="mt-2 sm:mt-6 mx-auto relative">
+              <div>
+                <div className="relative z-10 flex items-center space-x-3 p-3 border rounded-box shadow-lg bg-base-300 dark:border-0">
+                  <div className="flex-[1_0_0%] ">
+                    <label
+                      htmlFor="hs-search-article-1"
+                      className="block text-sm text-gray-700 font-medium dark:text-white"
+                    >
+                      <span className="sr-only">Type your shoutout</span>
+                    </label>
+                    <input
+                      type="text"
+                      id="hs-search-article-1"
+                      className="py-4 px-6 block w-full text-3xl border-transparent rounded-box outline-0 focus:border-blue-500 focus:ring-blue-500 bg-base-200"
+                      placeholder="Type your shoutout"
+                      onChange={e => setNewGreeting(e.target.value)}
+                    />
+                  </div>
+                  <div className="flex-[0_0_auto] ">
+                    <button
+                      className={`btn btn-primary border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none ${
+                        isPending ? "disabled" : ""
+                      }`}
+                      onClick={handleSendGreeting}
+                    >
+                      {isPending ? <span className="loading loading-spinner"></span> : <LucideArrowRight />}
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="font-medium text-md my-4">Fee: 0.01 ETH + Gas</div>
+
+              <div className="hidden md:block absolute bottom-0 start-0 translate-y-10 -translate-x-32">
+                <svg
+                  className="w-40 h-auto text-cyan-500"
+                  width="347"
+                  height="188"
+                  viewBox="0 0 347 188"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M4 82.4591C54.7956 92.8751 30.9771 162.782 68.2065 181.385C112.642 203.59 127.943 78.57 122.161 25.5053C120.504 2.2376 93.4028 -8.11128 89.7468 25.5053C85.8633 61.2125 130.186 199.678 180.982 146.248L214.898 107.02C224.322 95.4118 242.9 79.2851 258.6 107.02C274.299 134.754 299.315 125.589 309.861 117.539L343 93.4426"
+                    stroke="currentColor"
+                    strokeWidth="7"
+                    strokeLinecap="round"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <ContractData />
+
+        <div className="hidden w-full p-6">
+          <h1 className="text-left">
+            <span className="block text-2xl mb-2">Welcome to</span>
+            {/* <span className="block text-5xl font-bold">Batch 6</span> */}
+            <span className="inline-block text-6xl font-bold bg-clip-text bg-gradient-to-l from-blue-600 to-violet-500 text-transparent dark:from-blue-400 dark:to-violet-400">
+              Batch 6
+            </span>
+          </h1>
+          <p className="text-left text-lg">Get started by taking a look at your batch GitHub repository.</p>
+          <p className="text-lg flex gap-2 justify-center">
+            <span className="font-bold">Checked in builders count:</span>
+            {!checkedInCounter.isFetched ? (
+              <span className="loading" />
+            ) : (
+              <span>{checkedInCounter?.data?.toString() || 0}</span>
+            )}
+          </p>
+        </div>
+
+        <div className="hidden px-5">
           <h1 className="text-center">
             <span className="block text-2xl mb-2">Welcome to</span>
             <span className="block text-4xl font-bold">Batch 6</span>
@@ -30,7 +184,7 @@ const Home: NextPage = () => {
           </p>
         </div>
 
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
+        <div className="hidden flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <BugAntIcon className="h-8 w-8 fill-secondary" />
---
+++ packages/nextjs/components/assets/ContractData.tsx
@@ -0,0 +1,161 @@
+import { useEffect, useRef, useState } from "react";
+import { LucidePause, LucidePlay } from "lucide-react";
+import Marquee from "react-fast-marquee";
+import { useAnimationConfig, useScaffoldReadContract, useScaffoldWatchContractEvent } from "~~/hooks/scaffold-eth";
+
+const MARQUEE_PERIOD_IN_SEC = 5;
+
+export const ContractData = () => {
+  const [transitionEnabled, setTransitionEnabled] = useState(true);
+  const [isRightDirection, setIsRightDirection] = useState(false);
+  const [marqueeSpeed, setMarqueeSpeed] = useState(0);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const greetingRef = useRef<HTMLDivElement>(null);
+
+  const { data: totalCounter } = useScaffoldReadContract({
+    contractName: "Greetings",
+    functionName: "totalCounter",
+  });
+
+  const { data: currentGreeting, isLoading: isGreetingLoading } = useScaffoldReadContract({
+    contractName: "Greetings",
+    functionName: "greeting",
+  });
+
+  useScaffoldWatchContractEvent({
+    contractName: "Greetings",
+    eventName: "GreetingChange",
+    onLogs: () => {
+      console.log("Greeting event emitted");
+    },
+  });
+
+  const { showAnimation } = useAnimationConfig(totalCounter);
+
+  const showTransition = transitionEnabled && !!currentGreeting && !isGreetingLoading;
+
+  useEffect(() => {
+    if (transitionEnabled && containerRef.current && greetingRef.current) {
+      setMarqueeSpeed(
+        Math.max(greetingRef.current.clientWidth, containerRef.current.clientWidth) / MARQUEE_PERIOD_IN_SEC,
+      );
+    }
+  }, [transitionEnabled, containerRef, greetingRef]);
+
+  return (
+    <div className="relative flex flex-col justify-center items-center bg-[url('/assets/gradient-bg.png')] bg-[length:100%_100%] py-10 px-5 sm:px-0 lg:py-auto max-w-[100vw] overflow-hidden">
+      <div className="UnauthenticatedPage__SectionRight-sc-1ayouh5-4 dCkNVo absolute -z-10">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1440"
+          height="900"
+          fill="none"
+          viewBox="0 0 1440 900"
+          className="UnauthenticatedPage__PositionedBackground-sc-1ayouh5-5 fXNnuB"
+        >
+          <defs>
+            <linearGradient id="i0" x1="720" x2="720" y1="0" y2="1702.5" gradientUnits="userSpaceOnUse">
+              <stop stopColor="#EBF3FF" stopOpacity="0.32"></stop>
+              <stop offset="1" stopColor="#B9D4FE" stopOpacity="0.14"></stop>
+            </linearGradient>
+          </defs>
+          <path fill="#4700EB" d="M0 0h1440v900H0z"></path>
+          <path fill="url(#i0)" d="M0 0h1440v900H0z"></path>
+          <g fill="#FFFAEB" opacity="0.03">
+            <rect
+              width="327.93"
+              height="163.96"
+              x="583.5"
+              y="752.12"
+              rx="8"
+              transform="rotate(-180 583.5 752.12)"
+            ></rect>
+            <path d="M1067 196.93a7.85 7.85 0 0 0 8 7.9 331.5 331.5 0 0 0 118.22-24.86 330 330 0 0 0 107.01-71.09 327.9 327.9 0 0 0 71.5-106.39 326.3 326.3 0 0 0 25.01-117.5c.11-4.4-3.49-7.99-7.91-7.99h-148.91a8.32 8.32 0 0 0-8.2 8 163.1 163.1 0 0 1-12.36 54.75 163.7 163.7 0 0 1-35.75 53.19 164.9 164.9 0 0 1-53.5 35.54 165.7 165.7 0 0 1-55.11 12.3 8.3 8.3 0 0 0-8 8.18zM634.7 85.33a7.85 7.85 0 0 1 8-7.9c40.59.98 80.67 9.4 118.22 24.86a330 330 0 0 1 107.01 71.09 327.8 327.8 0 0 1 71.5 106.39 326.3 326.3 0 0 1 25 117.49c.12 4.42-3.48 8-7.9 8h-148.9a8.32 8.32 0 0 1-8.2-8 163.2 163.2 0 0 0-12.35-54.75 163.9 163.9 0 0 0-35.75-53.2 165 165 0 0 0-53.5-35.53 165.7 165.7 0 0 0-55.12-12.3 8.31 8.31 0 0 1-8-8.19V85.33z"></path>
+          </g>
+          <g fill="#F1EBFF" opacity="0.04">
+            <rect width="199.62" height="99.81" x="1331.81" y="303" rx="8" transform="rotate(90 1331.81 303)"></rect>
+            <path d="M1285.62 794.27a7.76 7.76 0 0 0-7.84 8 201.8 201.8 0 0 0 15.04 68.84 200.8 200.8 0 0 0 43.27 65.14 199.6 199.6 0 0 0 64.76 43.52 198.7 198.7 0 0 0 68.4 15.13c4.41.17 8-3.42 8-7.84v-84.4c0-4.41-3.59-7.96-8-8.32a99.3 99.3 0 0 1-30.2-7.32 99.8 99.8 0 0 1-32.38-21.76 100.5 100.5 0 0 1-21.64-32.57c-4-9.71-6.45-19.98-7.28-30.43-.35-4.4-3.9-7.99-8.32-7.99z"></path>
+            <rect width="199.62" height="99.81" x="878.38" y="538.94" rx="8"></rect>
+            <rect width="199.62" height="100.97" x="69.44" y="475.11" rx="8" transform="rotate(90 69.44 475.1)"></rect>
+            <path d="M544.24 157.1a7.76 7.76 0 0 0 7.84-8A199.62 199.62 0 0 0 360.62-42.35a7.76 7.76 0 0 0-8 7.84V49.3c0 4.42 3.59 7.97 8 8.32a99.8 99.8 0 0 1 62.58 28.92 99.8 99.8 0 0 1 28.91 62.58c.36 4.4 3.9 8 8.32 8h83.81zm36.7 801.67a7.76 7.76 0 0 0 7.99 7.84 199.62 199.62 0 0 0 176.43-123.07 199.6 199.6 0 0 0 15.03-68.4c.18-4.4-3.42-8-7.83-8h-83.82c-4.41 0-7.96 3.6-8.32 8a99.81 99.81 0 0 1-61.3 84.22 99.8 99.8 0 0 1-30.2 7.28c-4.4.35-7.99 3.9-7.99 8.32v83.81zM387.97 263.16a7.76 7.76 0 0 0-8-7.84 199.63 199.63 0 0 0-191.46 191.46 7.76 7.76 0 0 0 7.84 8h83.8c4.43 0 7.98-3.59 8.33-8a99.8 99.8 0 0 1 91.5-91.5c4.4-.34 8-3.9 8-8.31v-83.81z"></path>
+          </g>
+        </svg>
+      </div>
+
+      <div
+        className={`flex flex-col max-w-2xl bg-base-200 bg-opacity-70 rounded-md shadow-lg px-0 pb-4 w-full ${
+          showAnimation ? "animate-zoom" : ""
+        }`}
+      >
+        <div className="p-4 font-bold text-xl">Latest shoutout</div>
+        <div className="mt-0 border border-primary bg-base-300 rounded-md text-secondary  overflow-hidden text-[140px] whitespace-nowrap w-full uppercase tracking-tighter font-bai-jamjuree leading-tight">
+          <div className="relative overflow-x-hidden" ref={containerRef}>
+            {/* for speed calculating purposes */}
+            <div className="absolute -left-[9999rem]" ref={greetingRef}>
+              <div className="px-4 bg-clip-text bg-gradient-to-l from-blue-600 to-violet-500 text-transparent dark:from-blue-400 dark:to-violet-400">
+                {currentGreeting}
+              </div>
+            </div>
+            {new Array(3).fill("").map((_, i) => {
+              const isLineRightDirection = i % 2 ? isRightDirection : !isRightDirection;
+              return (
+                <Marquee
+                  key={i}
+                  direction={isLineRightDirection ? "right" : "left"}
+                  gradient={false}
+                  play={showTransition}
+                  speed={marqueeSpeed}
+                  className={i % 2 ? "overflow-y-hidden -my-10" : ""}
+                >
+                  <div className="px-4 overflow-y-hidden font-normal bg-clip-text bg-gradient-to-tl from-blue-600 to-violet-500 text-transparent dark:from-blue-400 dark:to-violet-400">
+                    {currentGreeting || " "}
+                  </div>
+                </Marquee>
+              );
+            })}
+          </div>
+        </div>
+
+        <div className="mt-6 flex justify-between px-4 w-full">
+          <button
+            className="btn btn-circle bg-base-100 relative bg-center"
+            onClick={() => {
+              setTransitionEnabled(!transitionEnabled);
+            }}
+          >
+            {transitionEnabled ? <LucidePause /> : <LucidePlay />}
+          </button>
+
+          <div className="bg-base-100/60 border border-primary rounded-xl flex items-center px-2">
+            <div className="p-2 py-1 border-r border-primary flex items-end">Total shoutouts: 📣</div>
+            <div className="text-4xl text-right min-w-[3rem] px-2 py-1 flex justify-end font-bai-jamjuree">
+              {totalCounter?.toString() || "0"}
+            </div>
+          </div>
+        </div>
+
+        <div className="hidden mt-3 flex items-end justify-between">
+          <button
+            className={`btn btn-circle btn-ghost border border-primary hover:border-primary w-12 h-12 p-1 bg-neutral flex items-center ${
+              isRightDirection ? "justify-start" : "justify-end"
+            }`}
+            onClick={() => {
+              if (transitionEnabled) {
+                setIsRightDirection(!isRightDirection);
+              }
+            }}
+          >
+            <div className="border border-primary rounded-full bg-secondary w-2 h-2" />
+          </button>
+          <div className="w-44 p-0.5 flex items-center bg-neutral border border-primary rounded-full">
+            <div
+              className="h-1.5 border border-primary rounded-full bg-secondary animate-grow"
+              style={{ animationPlayState: showTransition ? "running" : "paused" }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
